ee43ec56af7981f4c82c4f2c6cab3fa11fff72f8
Shrikant Pandey <shrikantpandeymnnit2015@gmail.com>
2018-06-07 10:09:44 +0530

    compress the string

---
+++ string/compress.py
@@ -0,0 +1,22 @@
+def compress(s):
+    r = ""
+    l = len(s)
+    if l == 0:
+        return ""
+    if l == 1:
+        return s+"l"
+    i=1
+    count=1
+    while i < l:
+        if s[i]==s[i-1]:
+            count+=1
+        else:
+            r = r+s[i-1]+str(count)
+            count=1
+        i +=1
+    r = r+s[i-1]+str(count)
+    return r
+
+
+s = "AAABBCDEEE"
+print(compress(s))
