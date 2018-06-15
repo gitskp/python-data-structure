e933a3eb03f84a0f058832c7756c8e2cc504406d
Shrikant Pandey <shrikantpandeymnnit2015@gmail.com>
2018-06-15 12:31:40 +0530

    decimal to binary

---
+++ basic/decimal_binary.py
@@ -0,0 +1,16 @@
+# Function to print binary number for the
+# input decimal using recursion
+def decimalToBinary(n):
+
+    if n > 1:
+         #  divide with integral result
+         # (discard remainder)
+        decimalToBinary(n//2)
+
+    # use print(n%2, end ='') for python 3
+    print n%2,
+
+# Driver code
+if __name__ == '__main__':
+    decimalToBinary(8)
+    print
