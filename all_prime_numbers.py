9335f02411aa7173d06b6d5d93be4e48c6f6f738
Shrikant Pandey <shrikantpandeymnnit2015@gmail.com>
2019-02-02 11:06:17 +0530

    find all prime numbers to n

---
+++ all_prime_numbers.py
@@ -0,0 +1,15 @@
+def all_prime_number(n):
+	prime = [True for i in range(n+1)]
+	p = 2
+	while(p*p<=n):
+		if prime[p]==True:
+			for i in range(p*p,n+1,p):
+				prime[i] = False
+		p +=1
+	for p in range(2,n):
+		if prime[p]:
+			print(p,end=' ')
+
+
+n = 15
+all_prime_number(n)
